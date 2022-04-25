84f8e45482198341dc73b2dd213687b5189d1996
Sterben01 <vocaloidmiku06@gmail.com>
2022-04-25 17:00:19 -0500

    Revert "add togglebutton"
    This reverts commit 9409043508f94f193a3692a821dead5bd2b5b1fd.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,6 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    addAndMakeVisible(togglebutton);
     setSize (600, 400);
 }
 
@@ -27,5 +26,4 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    togglebutton.setBounds(10, 10, 30, 30);
 }
